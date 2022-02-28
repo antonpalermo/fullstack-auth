89ea26e44aedd3c34bc9a1d9daf6a448f191c174
Anton Palermo <ANPBusinessmail@gmail.com>
2022-02-28 22:35:30 +0800

    feat: crud operation

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,15 +1,42 @@
-import { Body, Controller, Get, Post, Query } from '@nestjs/common'
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  NotAcceptableException,
+  Patch,
+  Post,
+  Query
+} from '@nestjs/common'
+import { InjectRepository } from '@nestjs/typeorm'
+import { Repository } from 'typeorm'
 import { AuthService } from './auth.service'
 import { CreateUserDto } from './dto/create-user.dto'
+import { UpdateUserDto } from './dto/update-user.dto'
+import { Account } from './entities/account.entity'
 import { User } from './entities/user.entity'
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    @InjectRepository(Account)
+    private readonly accountRepo: Repository<Account>
+  ) {}
 
   @Post('create')
   async createUser(@Body() user: CreateUserDto): Promise<User> {
     return await this.authService.create(user)
+  }
+
+  @Patch('update')
+  async updateUser(@Body() user: UpdateUserDto): Promise<User> {
+    return await this.authService.update({ user })
+  }
+
+  @Delete('delete')
+  async deleteUser(@Query('id') id: string) {
+    return await this.authService.delete(id)
   }
 
   // http://localhost:3000/auth/user?id="some_id"
@@ -19,6 +46,17 @@ export class AuthController {
     @Query('email') email?: string
   ): Promise<User> {
     return await this.authService.findOne({ where: [{ id }, { email }] })
+  }
+
+  @Get('account')
+  async account(@Query('provider_id') provider_id?: string): Promise<Account> {
+    const userAccount = await this.accountRepo.findOne({
+      where: { provider_account_id: provider_id },
+      relations: ['users']
+    })
+
+    if (!userAccount) throw new NotAcceptableException()
+    return userAccount ?? null
   }
 
   @Get('users')
--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm'
 import { FindOneOptions, Repository } from 'typeorm'
 import { CreateUserDto } from './dto/create-user.dto'
+import { UpdateAuthDto } from './dto/update-auth.dto'
 import { User } from './entities/user.entity'
 
 @Injectable()
@@ -13,6 +14,18 @@ export class AuthService {
 
   async create(user: CreateUserDto): Promise<User> {
     return await this.userRepo.save(user)
+  }
+
+  async update(user: UpdateAuthDto): Promise<User> {
+    return await this.userRepo.save({ ...user })
+  }
+
+  async delete(id: string) {
+    await this.userRepo.manager.transaction(async tx => {
+      await tx.delete('accounts', { id })
+      await tx.delete('sessions', { id })
+      await tx.delete('users', { id })
+    })
   }
 
   async findOne(options?: FindOneOptions<User>): Promise<User> {
